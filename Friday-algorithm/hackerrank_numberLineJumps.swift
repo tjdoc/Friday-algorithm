13d16f6b1a3d03368ecc1ba084f4e97c7cf9f0de
Tae-Jin Oh <taejinoh@gmail.com>
2020-11-20 18:25:24 +0900

    kangaroo - using closure

--- Friday-algorithm/hackerrank_numberLineJumps.swift
+++ Friday-algorithm/hackerrank_numberLineJumps.swift
@@ -8,14 +8,5 @@
 import Foundation
 
 func kangaroo(x1: Int, v1: Int, x2: Int, v2: Int) -> String {
-    let result: Bool
-    switch (x1, v1, x2, v2) {
-    case (x1, v1, x2, v2) where v2 >= v1:
-        result = false
-    case (x1, v1, x2, v2) where (x2-x1)%(v1-v2) != 0:
-        result = false
-    default:
-        result = true
-    }
-    return result ? "YES" : "NO"
+    return v2 >= v1 || (x2-x1)%(v1-v2) != 0 ? "NO" : "YES"
 }
